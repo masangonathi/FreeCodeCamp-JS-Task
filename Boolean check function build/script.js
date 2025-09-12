b9b80fb02530ca66ac741d962136cc72d9a4d53b
Nkosi <masangonim@gmail.com>
2025-09-12 09:56:12 +0200

    doing practice work

---
+++ Boolean check function build/script.js
@@ -0,0 +1,4 @@
+function booWho() {
+    return true;
+
+}
---
+++ script.js
@@ -0,0 +1,50 @@
+console.log("Hello! I'm your coding fun fact guide!")
+
+
+let botName = "Mark";
+let botLocation= "Vegas";
+let favoriteLanguage= "JavaScript";
+
+const NamePlace = "My name is " + botName + " and I live on " + botLocation + ".";
+
+let botName2 = "MathBot";
+let greeting = "Hi there! My name is " + botName2 + ".";
+console.log(greeting);
+
+console.log(5 === 2 + 3 || 4 === 2);
+
+
+if(1) {
+    console.log("True");
+}
+
+const a = 2;
+if(1 == "1") {
+    let b =3;
+    console.log(a + b);
+}
+
+
+let vehicle = "car";
+
+switch (vehicle) {
+  case "bike":
+    console.log("Bikes are two-wheelers.");
+    break;
+  case "car":
+    console.log("Some cars are 4x4.");
+  case "truck":
+    console.log("Trucks can carry heavy loads.");
+    break;
+  default:
+    console.log("Unknown vehicle.");
+}
+
+let x = 5;
+if (x > 1 && x < 10) {
+  console.log("x is between 1 and 10");
+} else {
+  console.log("x is not between 1 and 10");
+}
+
+
